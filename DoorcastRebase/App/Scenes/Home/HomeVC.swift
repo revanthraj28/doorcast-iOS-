3a298057eb82a5413fabd4e477f4666c069907eb
upendracodebele <93181745+upendracodebele@users.noreply.github.com>
2022-05-06 10:41:50 +0530

    changes in home vc

--- DoorcastRebase/App/Scenes/Home/HomeVC.swift
+++ DoorcastRebase/App/Scenes/Home/HomeVC.swift
@@ -9,15 +9,17 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    var viewModel : HomeViewModel!
     static var newInstance: HomeVC? {
         let storyboard = UIStoryboard(name: Storyboard.home.name,
                                       bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: self.className()) as? HomeVC
         return vc
     }
-    @IBOutlet weak var logoutButton: UIButton!
     
-    var viewModel : HomeViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Home Login info = \(SessionManager.loginInfo?.data?.accesstoken)")
@@ -35,10 +37,14 @@ class HomeVC: UIViewController {
     @IBAction func goToTaskDetailsButtonAction(_ sender: Any) {
         print("ert")
         guard let vc = CommonTaskDetailVC.newInstance else {return}
-        self.navigationController?.pushViewController(vc, animated: true)
-//        self.present(vc, animated: true, completion: nil)
+       // self.navigationController?.pushViewController(vc, animated: true)
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
     }
 }
+
+
+
 extension HomeVC : HomeViewModelProtocol {
     func organizationDetails(response: HomeModel) {
         print("Organization details = \(response)")
